6064e2ab513bc9899c9a61415c51d9426f0e5db8
Wyxwx <791989352@qq.com>
2019-04-14 17:05:02 +0800

    原生 API 使用

--- ribbon/src/main/java/com/example/demo/controller/HelloController.java
+++ ribbon/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
 
 /**
  * @Auther: wyx
@@ -16,10 +19,19 @@ public class HelloController {
 
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private LoadBalancerClient loadBalancerClient;
 
     @RequestMapping(value = "/hello")
     public String hello(){
         return restTemplate.getForObject("http://ribbonClient/hello", String.class);
+    }
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        ServiceInstance instance = loadBalancerClient.choose("ribbonClient");
+        URI uri = URI.create(String.format("http://%s:%s", instance.getHost(), instance.getPort()));
+        return "uri: " + uri;
     }
 
 }
